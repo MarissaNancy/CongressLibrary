b63b4dc2d244c2b0f0a43e0e0048e36330fc4ecd
Henry Parras <henry.parras@yahoo.com>
2021-05-25 14:00:54 -0700

    javascript2.0

--- script.js
+++ script.js
@@ -14,18 +14,37 @@
 //     source: formats,
 //   });
 var saveButton = document.querySelector("#bttn");
-var requestUrl = "https://www.loc.gov/"
-var searchParameters = document.querySelector("#formGroupExampleInput").value;
-var format = 
-console.log(searchParameters)
+var requestUrl = "https://www.loc.gov/search/?fo=json"
+
+
+
 
 saveButton.addEventListener("click", getApi);
   
-function getApi(){
+function getApi(event){
     
-    event.preventDefault
+    event.preventDefault()
+    var format = document.querySelector("#format-input").value
+    console.log(format)
+    var searchParameters = document.querySelector("#formGroupExampleInput").value
+    console.log(searchParameters)
+    
+    requestUrl = requestUrl + "&q=" + searchParameters + "&format=" + format
+    console.log(requestUrl)
+
+    
     fetch(requestUrl)
     .then(function (response) {
       return response.json();
+}) .then(function(data){
+
+    var results = data.results
+    console.log(results)
+
+    for (var i = 0;  i < results.length; i++) {
+        
+        console.log(results[i].facets.results.description);
+
+    }
 })
 }
